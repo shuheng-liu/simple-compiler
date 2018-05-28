a52a572666b632313ad41cf3f8d5a82c2e308586
Shuheng Johnny Liu <wish1104@outlook.com>
2018-05-28 22:22:08 +0800

    deleted
    Signed-off-by: Shuheng Johnny Liu <wish1104@outlook.com>

--- test_file/AbstractSyntaxTree.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, type: str, children: list = None, leaf=None):
-        self.type = type
-        self.children = children if children else list()
-        self.leaf = leaf
-
-    def traverse(self):
-        pass
